831561264b372911fca8d10f1d6190c6ac830ffc
Krist Menina <krist@quipper.com>
2022-12-01 15:16:42 +0800

    match rpc methods in generated code

--- test.go
+++ test.go
@@ -1,4 +1,4 @@
 package test
 
-//go:generate protoc --go_out=./build/         test.proto
-//go:generate protoc --gotemplate_out=template_dir=templates:./build/ test.proto
+//go:generate protoc --go_out=./build/         client.proto
+//go:generate protoc --gotemplate_out=template_dir=templates:./build/ client.proto
